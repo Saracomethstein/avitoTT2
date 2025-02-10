14c5ea160a42c2c05cd81f19fb64f4d7b9eda40a
Adel Sevastianov <102406080+Saracomethstein@users.noreply.github.com>
2025-02-10 16:23:32 +0300

    add readme file and my service struct

---
+++ internal/service/service.go
@@ -0,0 +1,10 @@
+package service
+
+type ApiService struct{}
+
+// func (s *ApiService) ApiInfoGet(ctx context.Context) (openapi.ImplResponse, error) {
+// 	return openapi.ImplResponse{
+// 		Code: http.StatusOK,
+// 		Body: map[string]string{"message": "API is working"},
+// 	}, nil
+// }
